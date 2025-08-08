32867ffa68e6f4dca6d34def5811b918ff4bd0c5
joe9wang <115807914+joe9wang@users.noreply.github.com>
2025-08-08 09:42:04 +0900

    Add files via upload

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,9 +1,9 @@
 
 export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCy-r6L1NgyHcqvsfpkyNPDJq9uMvv2CMM",
+  authDomain: "cardgame-f484c.firebaseapp.com",
+  projectId: "cardgame-f484c",
+  storageBucket: "cardgame-f484c.firebasestorage.app",
+  messagingSenderId: "248859224605",
+  appId: "1:248859224605:web:1320093856bc1861c174f4"
 };
