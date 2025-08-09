607115dce0fe86f335673297a9f119049cecc4b2
joe9wang <joe9bk@gmail.com>
2025-08-09 12:42:27 +0900

    new

--- firebaseConfig.js
+++
@@ -1,9 +0,0 @@
-
-export const firebaseConfig = {
-  apiKey: "AIzaSyCy-r6L1NgyHcqvsfpkyNPDJq9uMvv2CMM",
-  authDomain: "cardgame-f484c.firebaseapp.com",
-  projectId: "cardgame-f484c",
-  storageBucket: "cardgame-f484c.firebasestorage.app",
-  messagingSenderId: "248859224605",
-  appId: "1:248859224605:web:1320093856bc1861c174f4"
-};
