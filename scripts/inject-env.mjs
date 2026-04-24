24f3cb05169c6dd3c1298a31275f01ee50bf6e2a
joe9wang <joe9bk@gmail.com>
2026-04-24 12:44:15 +0900

    feat: add script to inject environment variables into build files and copy assets to dist directory

--- scripts/inject-env.mjs
+++ scripts/inject-env.mjs
@@ -4,7 +4,7 @@ import { dirname, join } from "node:path";
 import { fileURLToPath } from "node:url";
 
 const OUT_DIR = "./dist";
-const PAGES = ["./index.html", "./game.html", "./mypage.html", "./login.html", "./plans.html"];
+const PAGES = ["./index.html", "./game.html", "./lobby.html", "./mypage.html", "./login.html", "./plans.html"];
 const JS_MODULES = [
   "./scripts/game.module.js",
   "./scripts/firebase.init.js",
@@ -12,6 +12,8 @@ const JS_MODULES = [
   "./scripts/i18n.js",
   "./scripts/hp.js",
   "./scripts/room.module.js",
+  "./scripts/lobby.module.js",
+  "./scripts/utils.js",
 ];
 
 const DIRS = [
